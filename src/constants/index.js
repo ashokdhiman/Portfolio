b424e16cd91c1e22236a6d5ec1aab4725cdb1839
ashok <ashokdhiman21308@gmail.com>
2025-07-21 18:17:18 +0530

    Project section updated

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
-import project1 from "../assets/projects/project-1.jpg";
-import project2 from "../assets/projects/project-2.jpg";
-import project3 from "../assets/projects/project-3.jpg";
+import project1 from "../assets/projects/project-1.png";
+import project2 from "../assets/projects/project-2.png";
+import project3 from "../assets/projects/project-3.png";
 import project4 from "../assets/projects/project-4.jpg";
 
 export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like SpringBoot, MySQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
@@ -40,34 +40,35 @@ export const ABOUT_TEXT = `I am a full-stack developer with a strong passion for
 
 export const PROJECTS = [
   {
-    title: "Smart Healthcare Application",
+    title: "Smart Email Assistant",
     image: project1,
     description:
-      "The Smart Healthcare Application uses AI to provide personalized health insights, diagnostic recommendations, and telemedicine services, helping users manage their health effectively.",
-    technologies: ["React", "Next.js","SpringBoot", "MongoDB","Machine-learning"],
+      "A productivity-focused tool that integrates Gemini AI to help users draft, summarize, and respond to emails intelligently through a chat-based interface and Chrome Extension.",
+    technologies: ["React", "Gemini API", "Spring Boot", "Chrome Extension"],
   },
   {
-    title: "Trading App",
+    title: "TradeFluent",
     image: project2,
     description:
-    "A user-friendly trading app that enables seamless buying, selling, and tracking of stocks and financial assets with real-time market data.",
-    technologies: ["React", "SpringBoot", "MangoDB"],
+      "A full-stack crypto trading platform with real-time market data, secure fiat-to-crypto transactions, wallet management, portfolio tracking, and an AI chatbot for crypto queries.",
+    technologies: ["React", "Spring Boot", "MongoDB", "Tailwind CSS", "Gemini API"],
   },
   {
-    title: "Mawaii Chat App",
+    title: "Real-Time Facial Emotion Classifier",
     image: project3,
     description:
-    "AI-powered messaging platform for intuitive, personalized, and efficient communication.",
-    technologies: ["React", "Meterial-UI", "Node.js", "Firebase","Express","MongoDB"],
+      "A real-time facial emotion detection app using OpenCV and a CNN trained on the FER-2013 dataset, deployed with Streamlit for live predictions based on facial expressions.",
+    technologies: ["Python", "TensorFlow", "OpenCV", "Keras", "Streamlit"],
   },
   {
     title: "Portfolio Website",
     image: project4,
     description:
-    "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["React", "Tailwind", "Framer motion",],
+      "A dynamic personal portfolio website to showcase projects, technical skills, and contact details, built with animations and responsive design.",
+    technologies: ["React", "Tailwind CSS", "Framer Motion"],
   },
 ];
+
 
 export const CONTACT = {
   address: "Shillai Sirmaur HP 173027",
